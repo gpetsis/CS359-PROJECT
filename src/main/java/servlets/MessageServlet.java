f7c3da1665a5c61e7d5736e1f7bcf0ab9f09beb8
Nikos Lasithiotakis <csd4924@csd.uoc.gr>
2024-01-22 20:24:18 +0200

    [Nikos] Update Messages

--- src/main/java/servlets/MessageServlet.java
+++ src/main/java/servlets/MessageServlet.java
@@ -6,7 +6,9 @@
 package servlets;
 
 import database.EditMessagesTable;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -65,7 +67,8 @@ public class MessageServlet extends HttpServlet {
         //booking_id
         EditMessagesTable emt = new EditMessagesTable();
         try {
-            ArrayList<Message> messages = emt.databaseToMessage(1);
+            int booking_id = Integer.valueOf(request.getHeader("BookingId"));
+            ArrayList<Message> messages = emt.databaseToMessage(booking_id);
             ArrayList<String> messagesToString = new ArrayList<String>();
             String tempMessage;
             for (int i = 0; i < messages.size(); i++) {
@@ -90,6 +93,21 @@ public class MessageServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String requestString = "";
+        BufferedReader in = new BufferedReader(new InputStreamReader(request.getInputStream()));
+        String line = in.readLine();
+        while (line != null) {
+            requestString += line;
+            line = in.readLine();
+        }
+        System.out.println(requestString);
+        EditMessagesTable emt = new EditMessagesTable();
+        try {
+            emt.addMessageFromJSON(requestString);
+        } catch (ClassNotFoundException ex) {
+            response.setStatus(409);
+            Logger.getLogger(MessageServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
